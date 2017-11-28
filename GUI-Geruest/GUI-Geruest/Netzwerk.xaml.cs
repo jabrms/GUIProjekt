44c833a9770e20a729da1ce67d6bb81c4ab51a1f
jannik ostermayer <jannik.ostermayer@hsworms.onmicrosoft.com>
2017-11-28 21:24:19 +0100

    erste Schritte Netzwerk(Page)

--- GUI-Geruest/GUI-Geruest/Netzwerk.xaml.cs
+++ GUI-Geruest/GUI-Geruest/Netzwerk.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Management;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,26 +26,55 @@ namespace GUI_Geruest
         public Netzwerk()
         {
             InitializeComponent();
+            contentAendern();
         }
-        
-        //string[] Adressen;
-        //string[] SubnetMasken;
-        //string[] DefaultGateways;
-        //string NetworkCard;
-        //string MACAddress;
-        void NetzwerkInfo()
-        {     
-           // ManagementObjectSearcher NetworkInfo = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'");
-            //ManagementObjectCollection MOC = NetworkInfo.Get();
-            //foreach (ManagementObject mo in MOC)
-            //{
-            //    Adressen = (string[])mo["IPAddress"];
-            //    SubnetMasken = (string[])mo["IPSubnet"];
-            //    DefaultGateways = (string[])mo["DefaultIPGateway"];
 
-            //    NetworkCard = mo["Description"].ToString();
-            //    MACAddress = mo["MACAddress"].ToString();
-            //}
+        string[] ipAdressen;
+        string[] SubnetMasken;
+        string[] DefaultGateways;
+        string NetworkCard;
+        string MACAddress;
+        string hostname;
+        private void NetzwerkInfo()
+        {
+            ManagementObjectSearcher NetworkInfo = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'");
+            ManagementObjectCollection moCollection = NetworkInfo.Get();
+            foreach (ManagementObject mo in moCollection)
+            {
+                ipAdressen = (string[])mo["IPAddress"];
+                SubnetMasken = (string[])mo["IPSubnet"];
+                DefaultGateways = (string[])mo["DefaultIPGateway"];
+                NetworkCard = mo["Description"].ToString();
+                MACAddress = mo["MACAddress"].ToString();
+                hostname = mo["DNSHostName"].ToString();
+            }
         }
+        private void contentAendern()
+        {
+            NetzwerkInfo();
+            /*
+            foreach(string ipA in ipAdressen)
+            {
+                ip_info.Content += ipA + "\n";     
+            }
+
+            foreach (string subm in SubnetMasken)
+            {
+                sm_info.Content += subm + "\n";
+            }
+
+            foreach (string gw in DefaultGateways)
+            {
+                gw_info.Content += gw + "\n";
+            }
+            //TODO bei meht Details button
+            */
+            ip_info.Content = ipAdressen[0];
+            sm_info.Content = SubnetMasken[0];
+            gw_info.Content = DefaultGateways[0];
+            mac_info.Content = MACAddress;
+            desc_info.Content = NetworkCard;
+            hn_info.Content = hostname;
+         }
     }
 }
