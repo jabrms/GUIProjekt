e92713af1a2779dec969fe1119ea88219d33dde5
jannik ostermayer <jannik.ostermayer@hsworms.onmicrosoft.com>
2017-11-28 18:37:51 +0100

    Prozess kann jetzt beendet werden (aber alle mit dem Namen)

--- GUI-Geruest/GUI-Geruest/Prozesse.xaml.cs
+++ GUI-Geruest/GUI-Geruest/Prozesse.xaml.cs
@@ -1,23 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-//TODO Button Akzualisieren
-//TODO Liste sotieren
-//TODO aus Liste AUswählen
-
 
 namespace GUI_Geruest
 {
@@ -30,64 +14,23 @@ namespace GUI_Geruest
         {
             InitializeComponent();
             GetAllProcess();
-            //ListBoxProcess.SelectionMode = SelectionMode.Single;
         }
 
         Process[] process;
 
-        /*
-        private Process[] sortProcess(Process[] proc)
-        {
-            Process tauschen;
-            int laenge = proc.Length;
-            for (int i = 0; i < laenge; i++)
-            {
-                for (int x = i; x < laenge-1; x++)
-                {
-                    String stringA = proc[x].ToString().Substring(29).Trim(')');
-                    String stringB = proc[x + 1].ToString().Substring(29).Trim(')');
-                    //MessageBox.Show("\n" + stringA + " " + stringB + " " + "\n" + String.Compare(stringA, stringB, true));
-                    if (String.Compare(stringA, stringB, false) == -1)
-                    {
-                       tauschen = proc[x];
-                       proc[x] = proc[i];
-                       proc[i] = tauschen;
-                   }
-               }
-           }
-        return proc;
-        }
-        */
-
-        /*alt
-    private void GetAllProcess()
-    {
-
-        process = Process.GetProcesses();
-
-        ListBoxProcess.Items.Clear();
-        foreach (Process p in process)
-        {
-            ListBoxProcess.Items.Add(p.ProcessName);
-        }
-
-        anzahl.Content = "Anzahl laufender Prozesse: " + process.Length;
-    }
-    */
-        private void GetAllProcess()
+        private void GetAllProcess()    //Array aus allen Prozessen
         {
             process = Process.GetProcesses();
             int laenge = process.Length;
-            string[] ProcessName = new string[laenge];
+            string[] ProcessName = new string[laenge];  //String aus Prozessnamen zum sotieren
             for (int i = 0; i < laenge; i++)
             {
-                ProcessName[i] = process[i].ToString().Substring(28).Trim(')');
+                ProcessName[i] = process[i].ToString().Substring(28).Trim(')');     //Schneidet unnoetigen Inhalt ab, nur Prozessname
             }
-
             Array.Sort(ProcessName);
 
             ListBoxProcess.Items.Clear();
-            foreach (String p in ProcessName)
+            foreach (string p in ProcessName)
             {
                 ListBoxProcess.Items.Add(p);
             }
@@ -97,40 +40,40 @@ namespace GUI_Geruest
 
 
 
-        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)   //Auswahl Prozess aus Liste
         {
-            GetAllProcess();
+           ListBoxItem processSelected = ((sender as ListBox).SelectedItem as ListBoxItem);
         }
 
-        private void go_Click(object sender, RoutedEventArgs e)
+        private void go_Click(object sender, RoutedEventArgs e)     //Programm starten
         {
             programmStarten seite = new programmStarten();//TODO mit Using
-
-            //{
             if (seite.ShowDialog() == seite.DialogResult)
             {
                 GetAllProcess();
             }
-
-            //}
         }
-        private void kill_Click(object sender, RoutedEventArgs e)
+        private void kill_Click(object sender, RoutedEventArgs e)   //Programm beenden
         {
             try
             {
-                process[ListBoxProcess.SelectedIndex].Kill();
-                GetAllProcess();
+                Process[] toKill = Process.GetProcessesByName(ListBoxProcess.SelectedItem.ToString());
+                foreach (Process proc in toKill)
+                {
+                    proc.Kill();
+                    GetAllProcess();
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Meldung", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            GetAllProcess();
         }
 
-        private void aktualisieren_Click(object sender, RoutedEventArgs e)
+        private void aktualisieren_Click(object sender, RoutedEventArgs e)      //Programm aktualisieren
         {
             GetAllProcess();
-
         }
     }
 }
