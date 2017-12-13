2acc94f0bf181ada6f63accbc6209e73f13f3c09
jannik ostermayer <jannik.ostermayer@hsworms.onmicrosoft.com>
2017-12-13 10:42:04 +0100

    ereigniss fehler abgefangen

--- GUI-Geruest/GUI-Geruest/Ereignisse.xaml.cs
+++ GUI-Geruest/GUI-Geruest/Ereignisse.xaml.cs
@@ -25,25 +25,23 @@ namespace GUI_Geruest
     {
         List<eventLogListe> SystemList = new List<eventLogListe>();
         List<eventLogListe> ApplicationList = new List<eventLogListe>();
+        List<eventLogListe> gesamtList = new List<eventLogListe>();
 
         EventLog applicationAnzahl = new EventLog("Application");
         EventLog systemAnzahl = new EventLog("System");
-        List<eventLogListe> gesamtList = new List<eventLogListe>();
+        
 
         public Ereignisse()
         {
-           
             InitializeComponent();
-
             SystemList = eventlogRead(int.Parse(anzahlElemente.Text), "System");
-            ApplicationList = eventlogRead(int.Parse(anzahlElemente.Text), "Application");
-            //anzEreig.Content = "Anzahl der Ereignisse im Windows Log: " + (eventlogRead(10, "System")); //+eventlogRead(10, "Application") + eventlogRead(10, "Security") + eventlogRead(10, "Setup") + eventlogRead(10, "Forwarded Events"));
             listeSystem.ItemsSource = SystemList;
+            ApplicationList = eventlogRead(int.Parse(anzahlElemente.Text), "Application");
             listeApplication.ItemsSource = ApplicationList;
         }
 
-        
-       
+
+
 
         List<eventLogListe> eventlogRead(int anzahl, string protokoll)
         {
@@ -53,7 +51,7 @@ namespace GUI_Geruest
 
             EventLog log = new EventLog(protokoll);
 
-            gesamtAnzLogs.Content = "Anzahl Logs: " + log.Entries.Count;
+            gesamtAnzLogs.Content = "Anzahl " + protokoll + " Logs: " + log.Entries.Count;
 
             foreach (EventLogEntry entry in log.Entries)
             {
@@ -73,37 +71,42 @@ namespace GUI_Geruest
             return EventList;
         }
 
-        private void Application_DragOver(object sender, DragEventArgs e)
+        private void Application_DragOver(object sender, DragEventArgs e)   //TODO beschreibung Header
         {
          //    = "Das Anwendungsprotokoll enthält Ereignisse, die von Anwendungen oder Programmen protokolliert wurden. \nVon einem Datenbankprogramm könnte beispielsweise ein Dateifehler im Anwendungsprotokoll aufgezeichnet werden. \nDie Entwickler des jeweiligen Programms entscheiden, welche Ereignisse protokolliert werden.";
         }
 
         private void listeEreignisse_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-            var items = e.AddedItems;
-            if (items == null)
+            try
+            {
+                var items = e.AddedItems;
+                if (items == null)
+                {
+                    return;
+                }
+                eventLogListe selected = (eventLogListe)items[0]; //TODO wenn man nicht wartet fehler || hier auch fehler bei zhal eintippen und okay
+                message.Text = selected.message;
+                eventID.Content = selected.id.ToString();
+                typ.Content = selected.type;
+                logErst.Content = selected.timeWritte;
+                source.Content = selected.source;
+                user.Content = selected.user;
+                pc.Content = selected.pc;
+                zeit.Content = selected.timeGen;
+            }
+            catch
             {
                 return;
             }
-            eventLogListe selected = (eventLogListe)items[0];
-            message.Text = selected.message;
-            eventID.Content = selected.id.ToString();
-            typ.Content = selected.type;
-            logErst.Content = selected.timeWritte;
-            source.Content = selected.source;
-            user.Content = selected.user;
-            pc.Content = selected.pc;
-            zeit.Content = selected.timeGen;
+
         }
 
         void listeAktualisieren()
         {
             //TODO nur applikation
-            ApplicationList.Clear();
             ApplicationList = eventlogRead(int.Parse(anzahlElemente.Text), "Application");
             listeApplication.ItemsSource = ApplicationList;
-            SystemList.Clear();
             SystemList = eventlogRead(int.Parse(anzahlElemente.Text), "System");
             listeSystem.ItemsSource = SystemList;
         }
@@ -120,13 +123,22 @@ namespace GUI_Geruest
                 listeAktualisieren();
             }
         }
-        //private void anzahlElemente_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    ApplicationList.Clear();
-        //    eventlogRead(anzahlElemente.SelectedIndex, "Application");
-        //    listeApplication.ItemsSource = ApplicationList;
-        //}
-        ////TODO wie viele Events anzeigen
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)    //ohne denn code kein fehler
+        {
+            //string tabItem = ((sender as TabControl).SelectedItem as TabItem).Header as string;
+            //if (tabItem == "Application")
+            //{
+            //    ApplicationList = eventlogRead(int.Parse(anzahlElemente.Text), "Application");
+            //    listeApplication.ItemsSource = ApplicationList;
+            //}
+
+            //else if (tabItem == "System")
+            //{
+            //    SystemList = eventlogRead(int.Parse(anzahlElemente.Text), "System");
+            //    listeSystem.ItemsSource = SystemList;
+            //}
+        }
 
         //TODO Header sotieren implementieren
     }
