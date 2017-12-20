7761d470e34242d18ffa16b52c844f212968f0f7
jannik ostermayer <jannik.ostermayer@hsworms.onmicrosoft.com>
2017-12-20 12:38:17 +0100

    Hardware daten auslesen

--- GUI-Geruest/GUI-Geruest/Hardware.xaml.cs
+++ GUI-Geruest/GUI-Geruest/Hardware.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+
 
 namespace GUI_Geruest
 {
@@ -20,14 +26,50 @@ namespace GUI_Geruest
     /// </summary>
     public partial class Hardware : Page
     {
+        Computer pc;
+        DispatcherTimer perfCountTimer = new DispatcherTimer();
         public Hardware()
         {
             InitializeComponent();
+            pc = new Computer();
+            pc.CPUEnabled = true;
+            pc.FanControllerEnabled = true;
+            pc.GPUEnabled = true;
+            pc.HDDEnabled = true;
+            pc.RAMEnabled = true;
+            pc.Open();
+
+            //perfCountTimer.Tick += PerfCountTimer_Tick;
+            //perfCountTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            //perfCountTimer.IsEnabled = true;
+            PerfCountTimer_Tick();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void PerfCountTimer_Tick(/*object sender, EventArgs e*/)
         {
+            aktualisieren();
+        }
 
+            void aktualisieren()
+        {
+            foreach (var hardwareItem in pc.Hardware)
+            {
+                //if (hardwareItem.HardwareType == HardwareType.CPU)
+                //{
+                  
+                    hardwareItem.Update();
+
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                    {
+                        subHardware.Update();
+                    }                     
+
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {          
+                            temp.Content +=  "Hardware:" + hardwareItem.Name.ToString()+" Sensortype"+(sensor.SensorType.ToString() + " name:" + sensor.Name + " Value: " + sensor.Value.Value.ToString()) + "\n";
+                    } 
+                //}
+            }
         }
     }
 }
